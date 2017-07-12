b99ba5314db669f7574501a063a9cece12d65e6b
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-12 17:02:06 -0500

    template literal update

--- starter_files/customers.js
+++ starter_files/customers.js
@@ -5,26 +5,21 @@ console.log(customers.results);
 let container = document.querySelector('.container');
 for (let i = 0; i < customers.results.length; i++) {
   let person = customers.results[i];
-  let subContainer = document.createElement('div');
-  let img = document.createElement('img');
-  let name = document.createElement('span');
-  let email = document.createElement('p');
-  let info = document.createElement('p');
-  let ssn = document.createElement('p');
-  name.classList.add('name');
-  info.classList.add('info');
-  ssn.classList.add('ssn');
-  info.innerHTML = `<p>${person.location.street}<br />${person.location.city}, ${person.location.state} ${person.location.postcode}<br />${person.cell}</p>`
-  email.textContent = person.email;
-  email.classList.add('email');
-  name.textContent = `${person.name.title} ${person.name.first} ${person.name.last}`;
-  img.src = person.picture.large;
-  ssn.textContent = person.id.value;
-  subContainer.classList.add('sub-container');
-  subContainer.appendChild(img);
-  subContainer.appendChild(name);
-  subContainer.appendChild(email);
-  subContainer.appendChild(info);
-  subContainer.appendChild(ssn);
-  container.appendChild(subContainer)
+  let info = `<p>${person.location.street}<br />${person.location.city}, ${person.location.state} ${person.location.postcode}<br />${person.cell}</p>`
+  let email = person.email;
+  let name = `${person.name.title} ${person.name.first} ${person.name.last}`;
+  let imgSrc = person.picture.large;
+  let ssn = person.id.value;
+  container.innerHTML +=
+  `<div class="sub-container">
+    <img src="${imgSrc}">
+    <span class="name">${name}</span>
+    <p class="email">${email}</p>
+    <p class="info">
+      ${info}
+    </p>
+    <p class="ssn">
+        ${ssn}
+    </p>
+  </div>`
 }
